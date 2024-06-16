d6cab28fe188505c73b51c1ee4ad54163145b71b
Reljod Oreta <oretareljod@gmail.com>
2024-06-16 18:59:07 +0800

    Add plugin to preview markdown

--- init.lua
+++ init.lua
@@ -779,6 +779,15 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
