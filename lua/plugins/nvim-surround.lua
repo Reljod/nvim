ef85e2c20ccc3fe028f5e315481c269ac413faca
Reljod Oreta <oretareljod@gmail.com>
2024-07-18 22:14:56 +0800

    Use mini plugin to do nvim surround shortcuts

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -4,6 +4,7 @@ return {
   'kylechui/nvim-surround',
   version = "*", -- Use for stability; omit to use `main` branch for the latest features
   event = "VeryLazy",
-  opts = {}
+  opts = {},
+  enabled = false
 }
 
