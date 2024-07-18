f19170b1287ca0030a881fe0201b5d63f634f800
Reljod Oreta <oretareljod@gmail.com>
2024-07-18 22:28:11 +0800

    Add which-key plugin

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,22 @@
+return {
+  -- Shows the available keybindings as you type.
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  }
+}
