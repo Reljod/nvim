4ee1c87d328aab9290a9a085964102b83320f062
Reljod Oreta <oretareljod@gmail.com>
2024-07-26 23:53:29 +0800

    Add neo-tree mapping to focus only on explorer

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,15 @@ return {
       function()
         require('neo-tree.command').execute { toggle = true, dir = vim.uv.cwd() }
       end,
-      desc = 'Explorer NeoTree (Root Dir)',
+      desc = 'Explorer NeoTree (Current Dir)',
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Current Dir)", remap = true },
+    {
+      '<leader>fE',
+      function()
+        require('neo-tree.command').execute { action = "focus", dir = vim.uv.cwd() }
+      end,
+      desc = 'Close NeoTree',
     },
     {
       '<leader>ge',
@@ -66,8 +74,9 @@ return {
           end,
           desc = 'Copy Path to Clipboard',
         },
-        ['O'] = "system_open",
+        ['O'] = 'system_open',
         ['P'] = { 'toggle_preview', config = { use_float = false } },
+        ['D'] = "diff_files"
       },
     },
     default_component_configs = {
@@ -87,6 +96,34 @@ return {
     commands = {
       system_open = function()
         vim.fn.jobstart({ 'open', '.' }, { detach = true })
+      end,
+      diff_files = function(state)
+        local node = state.tree:get_node()
+        local log = require 'neo-tree.log'
+        state.clipboard = state.clipboard or {}
+        if diff_Node and diff_Node ~= tostring(node.id) then
+          local current_Diff = node.id
+          require('neo-tree.utils').open_file(state, diff_Node, open)
+          vim.cmd('vert diffs ' .. current_Diff)
+          log.info('Diffing ' .. diff_Name .. ' against ' .. node.name)
+          diff_Node = nil
+          current_Diff = nil
+          state.clipboard = {}
+          require('neo-tree.ui.renderer').redraw(state)
+        else
+          local existing = state.clipboard[node.id]
+          if existing and existing.action == 'diff' then
+            state.clipboard[node.id] = nil
+            diff_Node = nil
+            require('neo-tree.ui.renderer').redraw(state)
+          else
+            state.clipboard[node.id] = { action = 'diff', node = node }
+            diff_Name = state.clipboard[node.id].node.name
+            diff_Node = tostring(state.clipboard[node.id].node.id)
+            log.info('Diff source file ' .. diff_Name)
+            require('neo-tree.ui.renderer').redraw(state)
+          end
+        end
       end,
     },
   },
